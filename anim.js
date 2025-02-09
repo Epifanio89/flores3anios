d873c6ecc74f66345a982d809690505c94ac2ef3
EPIFANIO SERRANO BLAS <ing.serrano89@gmail.com>
2025-02-09 12:30:02 -0600

    letra musical

--- anim.js
+++ anim.js
@@ -7,48 +7,48 @@ var lyricsData = [
   { text: "I found a love for me", time: 1 },
   { text: "Oh, darling, just dive right in and follow my lead", time: 7 },
   { text: "Well, I found a girl, beautiful and sweet", time: 16 },
-  { text: "Oh, I never knew you were the someone waitin' for me.", time: 24 },
+  { text: "Oh, I never knew you were the someone waitin' for me", time: 24 },
 
-  { text: "'Cause we were just kids when we fell in love, not knowin' what it was", time: 33 },
-  { text: "I will not give you up this time", time: 41 },
+  { text: "'Cause we were just kids when we fell in love, not knowin' what it was", time: 30 },
+  { text: "I will not give you up this time", time: 40 },
   { text: "Oh, darling, just kiss me slow, your heart is all I own", time: 47 },
-  { text: "And in your eyes, you're holding mine", time: 54 },
+  { text: "And in your eyes, you're holding mine", time: 55 },
 
-  { text: "Baby, I'm dancin' in the dark with you between my arms", time: 59 },
-  { text: "Barefoot on the grass while listenin' to our favourite song", time: 67 },
-  { text: "When you said you looked a mess, I whispered underneath my breath", time: 72 },
-  { text: "But you heard it, Darling, you look perfect tonights", time: 78 },
+  { text: "Baby, I'm dancin' in the dark with you between my arms", time: 60 },
+  { text: "Barefoot on the grass while listenin' to our favourite song", time: 73 },
+  { text: "When you said you looked a mess, I whispered underneath my breath", time: 80 },
+  { text: "But you heard it, Darling, you look perfect tonights", time: 88 },
   
-  { text: "Well, I found a woman, stronger than anyone I know", time: 83 },
-  { text: "She shares my dreams, I hope that someday, I'll share her home", time: 91 },
-  { text: "I found a love to carry more than just my secrets", time: 97 },
-  { text: "To carry love, to carry children of our own", time: 104 },
+  { text: "Well, I found a woman, stronger than anyone I know", time: 100 },
+  { text: "She shares my dreams, I hope that someday, I'll share her home", time: 106 },
+  { text: "I found a love to carry more than just my secrets", time: 114 },
+  { text: "To carry love, to carry children of our own", time: 121 },
 
-  { text: "We are still kids, but we're so in love, fightin' against all odds", time: 108 },
-  { text: "I know we'll be alright this time", time: 144 },
-  { text: "Darling, just hold my hand, be my girl, I'll be your man", time: 148 },
-  { text: "I see my future in your eyes", time: 153 },
+  { text: "We are still kids, but we're so in love, fightin' against all odds", time: 129 },
+  { text: "I know we'll be alright this time", time: 138 },
+  { text: "Darling, just hold my hand, be my girl, I'll be your man", time: 145 },
+  { text: "I see my future in your eyes", time: 154 },
 
-  { text: "Oh, baby, I'm dancin' in the dark with you between my arms", time: 158 },
-  { text: "Barefoot on the grass while listenin' to our favourite song", time: 164 },
-  { text: "When I saw you in that dress, lookin' so beautiful", time: 169 },
-  { text: "I don't deserve this, darling, you look perfect tonight", time: 176 },
+  { text: "Oh, baby, I'm dancin' in the dark with you between my arms", time: 160 },
+  { text: "Barefoot on the grass while listenin' to our favourite song", time: 171 },
+  { text: "When I saw you in that dress, lookin' so beautiful", time: 178 },
+  { text: "I don't deserve this, darling, you look perfect tonight", time: 184 },
 
-  { text: "No, no, no, Mm, oh", time: 183 },
+  { text: "No, no, no, Mm, oh", time: 198 },
   
-  { text: "Baby, I'm dancin' in the dark with you between my arms", time: 158 },
-  { text: "Barefoot on the grass while listenin' to our favourite song", time: 164 },
-  { text: "I have faith in what I see, now I know I have met", time: 169 },
-  { text: "An angel in person, and she looks perfect", time: 176 },
+  { text: "Baby, I'm dancin' in the dark with you between my arms", time: 205 },
+  { text: "Barefoot on the grass while listenin' to our favourite song", time: 216 },
+  { text: "I have faith in what I see, now I know I have met", time: 224 },
+  { text: "An angel in person, and she looks perfect", time: 234 },
 
-  { text: "Though I don't deserve this, you look perfect tonight", time: 188 },
+  { text: "Though I don't deserve this, you look perfect tonight", time: 239 },
 ];
 
 // Animar las letras
 function updateLyrics() {
   var time = Math.floor(audio.currentTime);
   var currentLine = lyricsData.find(
-    (line) => time >= line.time && time < line.time + 6
+    (line) => time >= line.time && time < line.time + 8
   );
 
   if (currentLine) {
